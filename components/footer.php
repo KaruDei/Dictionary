71d0ee21ee6a96ccc5fb127b57c4391a3f19cae6
Просвернин Ярослав <1myoffice13@gmail.com>
2023-07-14 01:22:22 +0400

    Update footer.php file

---
+++ components/footer.php
@@ -0,0 +1,16 @@
+</main>
+<footer class="footer">
+	<p class="footer_logo">Словарь</p>
+	<div class="footer_menu">
+		<!-- <a class="hover" href="index.php">Главная</a> -->
+		<a class="hover" href="index.php">Английский</a>
+		<a class="hover" href="japan_page.php">Японский</a>
+		<a class="hover" href="tests_page.php">Тесты</a>
+	</div>
+	<p class="footer_data">&copy; 2023</p>
+</footer>
+</div>
+
+<script src="/assets/js/script.js"></script>
+</body>
+</html>
