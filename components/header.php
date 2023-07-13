62ddc2887aa4e0609453d6dc8fc3eb639f569634
Просвернин Ярослав <1myoffice13@gmail.com>
2023-07-14 01:22:05 +0400

    Update header.php file

---
+++ components/header.php
@@ -0,0 +1,28 @@
+<?php
+	require 'controllers/bd.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Dictionary</title>
+	<link rel="stylesheet" href="/assets/css/style.css">
+</head>
+<body>
+	<div class="wrapper">
+		
+	<header class="header">
+		<div class="header_logo">
+			<a href="index.php" class="logo">Словарь</a>
+		</div>
+		<div class="header_menu">
+			<!-- <a href="index.php" class="header_menu_item hover">Главная</a> -->
+			<a href="index.php" class="header_menu_item hover">Английский</a>
+			<a href="japan_page.php" class="header_menu_item hover">Японский</a>
+			<a href="tests_page.php" class="header_menu_item hover">Тесты</a>
+		</div>
+	</header>
+	<main class="main">
